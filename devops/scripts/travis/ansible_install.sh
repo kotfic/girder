20121a9c2eff0d51cb8b2cc49e344e6c53c677fb
Christopher Kotfila <chris.kotfila@kitware.com>
2016-08-18 14:39:54 -0400

    Syntax fails if modules not installed

--- devops/scripts/travis/ansible_install.sh
+++ devops/scripts/travis/ansible_install.sh
@@ -8,8 +8,6 @@ cat <<EOF > /tmp/inventory
 localhost
 EOF
 
-# Check syntax
-ansible-playbook -i /tmp/inventory ./devops/ansible/travis-playbook.yml --syntax-check
 
 # Set up configuration
 cat <<EOF > ~/.ansible.cfg
@@ -23,14 +21,15 @@ EOF
 ansible-galaxy install nodesource.node -p /tmp/roles
 ansible-galaxy install Stouts.mongodb -p /tmp/roles
 
-
-
 # Export variables needed by travis-playbook.yml
 export PR=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo `curl -s $PR | jq -r .head.ref`; fi)
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
 
+
+# Check syntax
+ansible-playbook -i /tmp/inventory ./devops/ansible/travis-playbook.yml --syntax-check
 
 # Provision localhost
 ansible-playbook -i /tmp/inventory ./devops/ansible/travis-playbook.yml -vv
