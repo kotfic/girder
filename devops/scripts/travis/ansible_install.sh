49e7fc4b1df386ea8a55773a139847be761d8706
Christopher Kotfila <chris.kotfila@kitware.com>
2016-08-18 14:22:22 -0400

    install required modules into /etc/ansible/roles

--- devops/scripts/travis/ansible_install.sh
+++ devops/scripts/travis/ansible_install.sh
@@ -11,7 +11,7 @@ export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRAN
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
 
-ansible-galaxy install nodesource.node
-ansible-galaxy install Stouts.mongodb
+sudo ansible-galaxy install nodesource.node
+sudo ansible-galaxy install Stouts.mongodb
 
 ansible-playbook -i /tmp/inventory ./devops/ansible/travis-playbook.yml -vv
