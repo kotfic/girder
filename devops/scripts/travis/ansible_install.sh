821fff12975baff754453abdf8d89d168bc14c26
Christopher Kotfila <chris.kotfila@kitware.com>
2016-08-18 14:35:33 -0400

    Add syntax Check, add host_key_checking=false

--- devops/scripts/travis/ansible_install.sh
+++ devops/scripts/travis/ansible_install.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
 set -e
+
+# Create a local inventory
 cat <<EOF > /tmp/inventory
 [girder]
 localhost
 EOF
 
+# Check syntax
+ansible-playbook -i /tmp/inventory tests/travis-playbook.yml --syntax-check
+
+# Set up configuration
+cat <<EOF > ~/.ansible.cfg
+[defaults]
+host_key_checking = False
+roles_path        = /tmp/roles:/etc/ansible/roles
+EOF
+
+
+# Install required roles
+ansible-galaxy install nodesource.node -p /tmp/roles
+ansible-galaxy install Stouts.mongodb -p /tmp/roles
+
+
+
+# Export variables needed by travis-playbook.yml
 export PR=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo `curl -s $PR | jq -r .head.ref`; fi)
 echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
 
-cat <<EOF > ~/.ansible.cfg
-[defaults]
-roles_path = /tmp/roles:/etc/ansible/roles
-EOF
 
-ansible-galaxy install nodesource.node -p /tmp/roles
-ansible-galaxy install Stouts.mongodb -p /tmp/roles
-
+# Provision localhost
 ansible-playbook -i /tmp/inventory ./devops/ansible/travis-playbook.yml -vv
