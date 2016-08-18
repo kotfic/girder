fe3fb63bd36c2174a2d317688a696e7749a5ea70
Christopher Kotfila <chris.kotfila@kitware.com>
2016-08-18 13:40:22 -0400

    Test getting branch reliably

--- devops/scripts/travis/ansible_install.sh
+++ devops/scripts/travis/ansible_install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 set -e
-# Pass
+cat <<EOF > /tmp/inventory
+[girder]
+localhost
+EOF
+
+These contents will
+export PR=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo `curl -s $PR | jq -r .head.ref`; fi)
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH, PR=$PR, BRANCH=$BRANCH"
+echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR"
+
+# ansible-playbook -i /tmp/inventory ./devops/ansible/travis-playbook.yml -vv
